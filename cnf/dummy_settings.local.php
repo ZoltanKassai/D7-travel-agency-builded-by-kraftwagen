6abcfbd0a527bba7970bbee63259543fabde448e
spykspy.com <zoltankassai8@gmail.com>
2016-01-25 09:00:50 +0100

    Initial.

---
+++ cnf/dummy_settings.local.php
@@ -0,0 +1,17 @@
+<?php
+
+$databases = array (
+  'default' => array (
+    'default' => array (
+      'database' => '',
+      'username' => '',
+      'password' => '',
+      'host'     => '',
+      'port'     => '',
+      'driver'   => '',
+      'prefix'   => '',
+    ),
+  ),
+);
+
+$drupal_hash_salt = 'SOME_REALLY_RANDOM_STRING';
